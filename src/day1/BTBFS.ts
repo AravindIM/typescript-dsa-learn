b97374edf5f48db922dfdfad61bbd6fde3d387d0
Aravind I M <aravindim@yahoo.com>
2023-08-13 16:58:24 +0530

    feat(algo): Binary Tree BFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+	const q: BinaryNode<number>[] = [head];
 
+	while(q.length) {
+		const next = q.shift() as BinaryNode<number>;
+		
+		// search
+		if(next.value === needle) {
+			return true;
+		}
+
+		if(next.left) {
+			q.push(next.left);
+		}
+		
+		if(next.right) {
+			q.push(next.right);
+		}
+	}
+
+	return false;
 }
